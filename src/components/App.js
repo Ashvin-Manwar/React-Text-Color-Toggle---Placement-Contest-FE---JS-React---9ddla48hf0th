739773918bd8ffbf8f4eef51f9c091c888810efd
Ashvin Manwar <manwar.ashvin@gmail.com>
2023-10-01 12:53:58 +0000

    sub

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,18 @@ import React, {useEffect, useState} from 'react'
 import '../styles/App.css';
 
 const App = () => {
+  const [textColor,setTextColor]=useState(red);
+  function toggleButton(e){
+    const newColor=textColor===red ?blue: red;
+    setTextColor(newColor);
+  }
+  // constructor
+  const 
 //code here 
   return (
     <div id="main">
-      <p className={} >Newton School</p>
-      <button id='button' onClick={}>Change Style</button>
+      <p className={ma} >Newton School</p>
+      <button id='button' onClick={toggleButton}>Change Style</button>
     </div>
   )
 }
